ef3e6af1f1ffe561d3a5dca8b52e69c5577f7607
Lucas Inacio <lucasinacio2003@hotmail.com>
2025-10-19 19:12:37 -0300

    Delete wrong files from when I hadn't understand the assignment yet

--- src/main/java/com/rip/ManagerInterface.java
+++
@@ -1,6 +0,0 @@
-package com.rip;
-
-import com.routingmanagement.RoutingProtocolManagementInterface;
-
-public interface ManagerInterface extends UnicastServiceUserInterface, RoutingProtocolManagementInterface {
-}
--- src/main/java/com/rip/NodeInterface.java
+++
@@ -1,4 +0,0 @@
-package com.rip;
-
-public interface NodeInterface extends UnicastServiceUserInterface {
-}
